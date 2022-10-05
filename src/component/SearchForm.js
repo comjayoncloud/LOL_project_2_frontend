90796e72db934c12068950b5026e9e056c1ee6f3
jaewoo <comjayoncloud@gmail.com>
2022-10-05 22:38:46 +0900

    뼈대 만드는중2

--- src/component/SearchForm.js
+++ src/component/SearchForm.js
@@ -1,6 +1,14 @@
+import useFetch from "../hooks/useFetch";
 import "./SearchForm.css";
 
 export default function SearchForm() {
+  // const jaewoo = useFetch(
+  //   "https://www.lolproject-bakcend.com/api/allinfo?id=빵뒤를흔드록바"
+  // );
+  function handleSubmit(e) {
+    console.log("clicked submit", e.target.value);
+    e.preventDefault();
+  }
   return (
     <div className="SearchForm">
       <div className="title">
@@ -8,10 +16,12 @@ export default function SearchForm() {
 
         <p>Enter username in search bar using forramts username</p>
       </div>
-      <form className="f-search">
-        <div className="f-input">
-          <input type="text" placeholder="Enter ur username"></input>
-          <button className="search-button"></button>
+      <form className="f-search" onSubmit={handleSubmit}>
+        <div className="f-container">
+          <div className="f-input">
+            <input type="text" placeholder="Enter ur username"></input>
+            <button className="search-button"></button>
+          </div>
         </div>
       </form>
     </div>
